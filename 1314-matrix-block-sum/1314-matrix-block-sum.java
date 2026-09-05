37d4533797c3e46b6c8bda725fc32725868908b4
Krishna <k84497838@gmail.com>
2026-09-05 16:33:37 +0530

    Time: 7 ms (60.22%), Space: 46.9 MB (22.91%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -1,23 +1,35 @@
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
-        int n = mat.length;
-        int m = mat[0].length;
+        int m = mat.length;
+        int n = mat[0].length;
 
-        int[][] ans = new int[n][m];
+        int[][] prefix = new int[m+1][n+1];
+        
+        // build prefix matrix
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                prefix[i][j] = mat[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
+            }
+        }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                int sum = 0;
+        int ans[][] = new int[m][n];
 
-                for(int r = i - k; r <= i + k; r++) {
-                    for(int c = j - k; c <= j + k; c++) {
-                        if(r >= 0 && r < n && c >= 0 && c < m) {
-                            sum += mat[r][c];
-                        }
-                    }
-                }
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                int r1 = Math.max(0, i-k);
+                int c1 = Math.max(0, j-k);
+                int r2 = Math.min(m-1, i+k);
+                int c2 = Math.min(n-1, j+k);
 
-                ans[i][j] = sum;
+                r1++;
+                r2++;
+                c1++;
+                c2++;
+
+                ans[i][j] = prefix[r2][c2] 
+                            - prefix[r1-1][c2]
+                            - prefix[r2][c1-1]
+                            + prefix[r1-1][c1-1];
             }
         }
         return ans;
