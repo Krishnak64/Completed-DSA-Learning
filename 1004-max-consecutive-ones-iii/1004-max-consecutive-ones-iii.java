07cc54686ae6395976b2436d5693eb9bc3b62268
Krishna <k84497838@gmail.com>
2026-09-12 15:39:10 +0530

    Time: 37 ms (5.86%), Space: 55.5 MB (6.41%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxLen = 0;
+        int left = 0;
+
+        for(int right = 0; right < n; right++) {
+            int currentEle = nums[right];
+            map.put(currentEle, map.getOrDefault(currentEle, 0) + 1);
+
+            int zeroes = map.getOrDefault(0, 0);
+
+            while(zeroes > k) {
+                int previouseEle = nums[left];
+                map.put(previouseEle, map.getOrDefault(previouseEle, 0) - 1);
+                left++;
+                zeroes = map.getOrDefault(0, 0);
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
